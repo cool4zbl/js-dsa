7ce236a4c2f41c0de48425bce22d2122343490a5
cool4zbl <i@zhangbinliu.me>
2020-12-26 23:54:29 +0800

    add: throttle

--- throttle.js
+++ throttle.js
@@ -1,1 +1,22 @@
 // throttle
+
+function throttle(func, wait) {
+  let isCalled = false
+  let lastArgs
+
+  return function throttleInner() {
+    if (!isCalled) {
+      isCalled = true
+      func(...arguments)
+
+      setTimeout(() => {
+        isCalled = false
+        if (lastArgs) {
+          func(...lastArgs)
+        }
+      }, wait)
+    } else {
+      lastArgs = arguments
+    }
+  }
+}
