1274b3c2e3779cad44daf27dc9b6da0fc33e66c1
cool4zbl <i@zhangbinliu.me>
2020-12-27 12:00:15 +0800

    add `throttle-with-trailing-and-leading`

--- throttle.js
+++ throttle.js
@@ -1,16 +1,21 @@
-// throttle
+// implement basic throttle
+// @see https://bigfrontend.dev/problem/implement-basic-throttle
 
 function throttle(func, wait) {
-  let isCalled = false
+  // 1. 如果不是 cool time , 则调用 func, 并且重置冷却时间
+  //    1.2 冷却时间过了后，检查是否有额外的缓存 `arguments`, 如果有，则直接调用
+  // 2. 如果是 cool time, 则把当前 `arguments` 缓存起来
+
+  let isCooling = false
   let lastArgs
 
   return function throttleInner() {
-    if (!isCalled) {
-      isCalled = true
+    if (!isCooling) {
+      isCooling = true
       func(...arguments)
 
       setTimeout(() => {
-        isCalled = false
+        isCooling = false
         if (lastArgs) {
           func(...lastArgs)
         }
